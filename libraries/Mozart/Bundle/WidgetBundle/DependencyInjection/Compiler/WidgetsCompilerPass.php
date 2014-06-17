bb8fd7087719fbb6cc09fcb1c5b7f2a56dc1b2a4
Alexandru Furculita <alex@rhetina.com>
2014-06-18 01:14:50 +0300

    Added WidgetsCompilerPass

---
+++ libraries/Mozart/Bundle/WidgetBundle/DependencyInjection/Compiler/WidgetsCompilerPass.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\WidgetBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+class WidgetsCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function process( ContainerBuilder $container )
+    {
+        if (false === $container->hasDefinition( 'mozart_widget.widget_chain' )) {
+            return;
+        }
+
+        $definition = $container->getDefinition(
+            'mozart_widget.widget_chain'
+        );
+
+        foreach ($container->findTaggedServiceIds( 'wordpress.widget' ) as $id => $attributes) {
+            $definition->addMethodCall(
+                'registerWidget',
+                array( new Reference( $id ) )
+            );
+        }
+    }
+
+} 
