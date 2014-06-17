f7cf70b91a1a3a1c0f05fc98c89dc0481d077f32
Alexandru Furculita <alex@rhetina.com>
2014-06-17 20:46:25 +0300

    Added WidgetBundle

---
+++ libraries/Mozart/Bundle/WidgetBundle/MozartWidgetBundle.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\WidgetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MozartWidgetBundle extends Bundle {
+
+} 
