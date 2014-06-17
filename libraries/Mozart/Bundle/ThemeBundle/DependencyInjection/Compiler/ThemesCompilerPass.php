691f53f741b9e332051c9022172a3746038685d2
Alexandru Furculita <alex@rhetina.com>
2014-06-18 01:15:38 +0300

    Added ThemesCompilerPass

---
+++ libraries/Mozart/Bundle/ThemeBundle/DependencyInjection/Compiler/ThemesCompilerPass.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\ThemeBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+class ThemesCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function process( ContainerBuilder $container )
+    {
+        if (false === $container->hasDefinition( 'mozart_theme.theme_chain' )) {
+            return;
+        }
+
+        $definition = $container->getDefinition(
+            'mozart_theme.theme_chain'
+        );
+
+        foreach ($container->findTaggedServiceIds( 'wordpress.theme' ) as $id => $attributes) {
+            $definition->addMethodCall(
+                'registerTheme',
+                array( new Reference( $id ) )
+            );
+        }
+    }
+
+} 
