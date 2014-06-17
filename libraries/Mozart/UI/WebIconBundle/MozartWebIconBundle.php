4c069ccb43580b8836a3d7f343ee7f5f35e9f158
Alexandru Furculita <alex@rhetina.com>
2014-06-18 00:35:32 +0300

    UI components cleaned

--- libraries/Mozart/UI/WebIconBundle/MozartWebIconBundle.php
+++ libraries/Mozart/UI/WebIconBundle/MozartWebIconBundle.php
@@ -2,9 +2,29 @@
 
 namespace Mozart\UI\WebIconBundle;
 
-use Mozart\Component\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class MozartWebIconBundle
+ *
+ * @package Mozart\UI\WebIconBundle
+ */
 class MozartWebIconBundle extends Bundle
 {
-    //put your code here
+    /**
+     *
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build( ContainerBuilder $container )
+    {
+        parent::build( $container );
+    }
 }
