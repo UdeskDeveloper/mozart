1e1410b654545d235a26ec2822235057f036f924
Alexandru Furculita <alex@rhetina.com>
2014-06-23 16:17:17 +0300

    Update MozartBackofficeBundle.php

--- libraries/Mozart/Bundle/BackofficeBundle/MozartBackofficeBundle.php
+++ libraries/Mozart/Bundle/BackofficeBundle/MozartBackofficeBundle.php
@@ -13,7 +13,15 @@ use Mozart\Bundle\BackofficeBundle\DependencyInjection\Compiler\ReduxSectionsCom
  */
 class MozartBackofficeBundle extends Bundle
 {
+    /**
+     * @var Redux\Configuration
+     */
     protected $optionsManager;
+    /**
+     * @var Redux\Extensions\Configuration
+     */
+    protected $optionsExtensionManager;
+
     /**
      * @param ContainerBuilder $container
      */
@@ -24,17 +32,13 @@ class MozartBackofficeBundle extends Bundle
     }
 
     /**
-     *
+     * Boot the bundle
      */
     public function boot()
     {
         if (\Mozart::isWpRunning() === false) {
             return;
         }
-        add_image_size('admin-thumb', 80, 9999);
-        add_action('login_head', array(&$this, 'login_head'));
-        add_action('admin_head', array(&$this, 'load_styles'));
-        add_action('admin_menu', array(&$this, 'admin_menu_separator'));
 
         $this->optionsManager = new Redux\Configuration();
         $this->optionsManager->init(array(), $this->container);
@@ -44,73 +48,4 @@ class MozartBackofficeBundle extends Bundle
 
         $this->redux = new Redux\Redux();
     }
-
-
-    /**
-     *
-     */
-    public function login_head()
-    {
-        if (parameter('admin', 'login_screen', 'default') != 'on') {
-            echo "
-        <style>
-        body.login #login h1 a {
-            background: url('/wp-content/mozart/public/bundles/mozartbackoffice/img/admin.png') no-repeat scroll center top transparent;
-            height: 241px;
-        width: auto;
-        }
-        </style>";
-        }
-        if (parameter('admin', 'login_screen', 'image')) {
-            echo "
-        <style>
-        body.login #login h1 a {
-            background: url('" . parameter('admin', 'login_screen', 'image') . "') no-repeat scroll center top transparent;
-            height: 241px;
-        }
-        </style>";
-        }
-    }
-
-    /**
-     *
-     */
-    public function load_styles()
-    {
-        if (is_admin()) {
-            wp_register_style('admin-css', get_stylesheet_directory_uri()
-                    . '/wp-content/mozart/public/bundles/mozartbackoffice/css/admin.css');
-            wp_enqueue_style('admin-css');
-        }
-    }
-
-    /**
-     * @param $position
-     */
-    public function add_admin_menu_separator($position)
-    {
-        global $menu;
-        $index = 0;
-
-        foreach ($menu as $offset => $section) {
-            if (substr($section[2], 0, 9) == 'separator')
-                $index ++;
-            if ($offset >= $position) {
-                $menu[$position] = array('', 'read', "separator{$index}", '', 'wp-menu-separator');
-                break;
-            }
-        }
-
-        ksort($menu);
-    }
-
-    /**
-     *
-     */
-    public function admin_menu_separator()
-    {
-        $this->add_admin_menu_separator(30);
-        $this->add_admin_menu_separator(50);
-    }
-
 }
