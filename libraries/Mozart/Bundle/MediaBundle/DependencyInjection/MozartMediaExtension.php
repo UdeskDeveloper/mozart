a76bd027bb1ab771fe99e8f9ef3c21e34f982bf8
Alexandru Furculita <alex@rhetina.com>
2014-06-23 21:51:26 +0300

    MediaBundle completed

---
+++ libraries/Mozart/Bundle/MediaBundle/DependencyInjection/MozartMediaExtension.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\MediaBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class MozartMediaExtension extends Extension {
+
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $config An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
