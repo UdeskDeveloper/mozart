bcfe6909600e7831cf6fa1cc9cdb48ef1b1130a6
Alexandru Furculita <alex@rhetina.com>
2014-06-23 22:41:37 +0300

    Update MozartWidgetBundle.php

--- libraries/Mozart/Bundle/WidgetBundle/MozartWidgetBundle.php
+++ libraries/Mozart/Bundle/WidgetBundle/MozartWidgetBundle.php
@@ -5,8 +5,53 @@
 
 namespace Mozart\Bundle\WidgetBundle;
 
+use MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\stdClass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Mozart\Bundle\WidgetBundle\DependencyInjection\Compiler\WidgetsCompilerPass;
 
-class MozartWidgetBundle extends Bundle {
+/**
+ * Class MozartWidgetBundle
+ * @package Mozart\Bundle\WidgetBundle
+ */
+class MozartWidgetBundle extends Bundle
+{
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new WidgetsCompilerPass());
+    }
+
+    /**
+     *
+     */
+    public function boot()
+    {
+        add_action('widgets_init', array($this, 'registerWidgets'), 0);
+    }
+
+    /**
+     *
+     */
+    public function registerWidgets()
+    {
+
+        if (false === $this->container->has('mozart_widget.widget_chain')) {
+            return;
+        }
+        global $wp_widget_factory;
+
+        $widgets = $this->container->get('mozart_widget.widget_chain')->getWidgets();
+
+        foreach ($widgets as $name => $widget) {
+            $wp_widget_factory->widgets[get_class($widget)] = $widget;
+            // register_widget($widgetClass);
+        }
+    }
+
 
 } 
