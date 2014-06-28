d01f5144bb96d8e71dff8ff23bafa2071ce0ac46
Alexandru Furculita <alex@rhetina.com>
2014-06-28 13:11:15 +0300

    UserInterface completed

--- libraries/Mozart/Bundle/UserBundle/Model/UserInterface.php
+++ libraries/Mozart/Bundle/UserBundle/Model/UserInterface.php
@@ -1,10 +1,28 @@
 <?php
 
-namespace  Mozart\Bundle\UserBundle\Model;
+namespace Mozart\Bundle\UserBundle\Model;
 
 use Symfony\Component\Security\Core\User\UserInterface as SymfonyUserInterface;
 
+/**
+ * Interface UserInterface
+ *
+ * @package Mozart\Bundle\UserBundle\Model
+ */
 interface UserInterface extends SymfonyUserInterface
 {
+    /**
+     * @return mixed
+     */
+    public function getDisplayName();
 
+    /**
+     * @return mixed
+     */
+    public function getUrl();
+
+    /**
+     * @return mixed
+     */
+    public function getEmail();
 }
