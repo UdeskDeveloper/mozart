b0ff12f8732c8abca652545520b517a2d0327e1b
Alexandru Furculita <alex@rhetina.com>
2014-06-29 21:26:47 +0300

    Added Redux initializer

--- libraries/Mozart/Bundle/OptionBundle/MozartOptionBundle.php
+++ libraries/Mozart/Bundle/OptionBundle/MozartOptionBundle.php
@@ -3,10 +3,50 @@
  * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
  */
 
-namespace  Mozart\Bundle\OptionBundle;
+namespace Mozart\Bundle\OptionBundle;
 
+use Mozart\Bundle\OptionBundle\DependencyInjection\Compiler\ReduxSectionsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class MozartOptionBundle extends Bundle {
+/**
+ * Class MozartOptionBundle
+ *
+ * @package Mozart\Bundle\OptionBundle
+ */
+class MozartOptionBundle extends Bundle
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build( ContainerBuilder $container )
+    {
+        parent::build( $container );
+        $container->addCompilerPass( new ReduxSectionsCompilerPass );
+    }
+
+    /**
+     *
+     */
+    public function boot()
+    {
+        $this->runRedux();
+    }
+
+    /**
+     *
+     */
+    protected function runRedux()
+    {
+        //  var_dump($this->container->get( 'redux.sectionmanager' )->getSections());
+        $this->container->get( 'redux.configuration' )->init(
+            array(
+                'sections' => $this->container->get( 'redux.sectionmanager' )->getSections()
+            )
+        );
+
+        $this->container->get( 'redux.extensions.configuration' )->init();
+        $this->container->get( 'redux' )->init();
+    }
 
 } 
