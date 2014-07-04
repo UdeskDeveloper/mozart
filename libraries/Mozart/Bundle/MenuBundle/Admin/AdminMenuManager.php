3aa1e6f201cda56be0197265a31552ce755ac627
Alexandru Furculita <alex@rhetina.com>
2014-07-04 21:52:19 +0300

    MenuBundle added

---
+++ libraries/Mozart/Bundle/MenuBundle/Admin/AdminMenuManager.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\MenuBundle\Admin;
+
+
+/**
+ * Class AdminMenuManager
+ *
+ * @package Mozart\Bundle\MenuBundle\Admin
+ */
+class AdminMenuManager
+{
+    /**
+     * @var AdminMenuInterface[]
+     */
+    protected $menus;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->menus = array();
+
+    }
+
+    /**
+     * @param AdminMenuInterface $menu
+     */
+    public function addMenu( AdminMenuInterface $menu )
+    {
+        $this->menus[$menu->getAlias()] = $menu;
+    }
+
+    /**
+     * @return AdminMenuInterface[]
+     */
+    public function getMenus()
+    {
+        return $this->menus;
+    }
+
+    /**
+     * @param $alias
+     *
+     * @return AdminMenuInterface
+     */
+    public function getMenu( $alias )
+    {
+        return $this->menus[$alias];
+    }
+
+} 
---
+++ libraries/Mozart/Bundle/MenuBundle/DependencyInjection/MozartMenuExtension.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\MenuBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+class MozartMenuExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array            $config    An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load( array $config, ContainerBuilder $container )
+    {
+        $loader = new YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load( 'services.yml' );
+    }
+} 
