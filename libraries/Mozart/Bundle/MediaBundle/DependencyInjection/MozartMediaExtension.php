f70cf6b4f759a2106c128075f477ece9534cb033
Alexandru Furculita <alex@rhetina.com>
2014-07-04 21:52:43 +0300

    MediaBunde Updated

--- libraries/Mozart/Bundle/MediaBundle/DependencyInjection/MozartMediaExtension.php
+++ libraries/Mozart/Bundle/MediaBundle/DependencyInjection/MozartMediaExtension.php
@@ -5,23 +5,27 @@
 
 namespace Mozart\Bundle\MediaBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class MozartMediaExtension extends Extension {
-
+class MozartMediaExtension extends Extension
+{
     /**
      * Loads a specific configuration.
      *
-     * @param array $config An array of configuration values
+     * @param array            $config    An array of configuration values
      * @param ContainerBuilder $container A ContainerBuilder instance
      *
      * @throws \InvalidArgumentException When provided tag is not defined in this extension
      *
      * @api
      */
-    public function load(array $config, ContainerBuilder $container)
+    public function load( array $config, ContainerBuilder $container )
     {
-        // TODO: Implement load() method.
+        $loader = new YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load('services.yml');
+
     }
 }
