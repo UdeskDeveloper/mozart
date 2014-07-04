7e97ec0b6b35b1a1f9a1822a7cd0976a0cb305f7
Alexandru Furculita <alex@rhetina.com>
2014-07-04 21:50:12 +0300

    AssetBundle added

---
+++ libraries/Mozart/Bundle/AssetBundle/MozartAssetBunde.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\AssetBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MozartAssetBunde extends Bundle {
+
+} 
