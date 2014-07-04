be2125b718eca378df84bad6984f2c76b01c74e3
Alexandru Furculita <alex@rhetina.com>
2014-07-04 21:48:50 +0300

    AjaxBundle Added

---
+++ libraries/Mozart/Bundle/AjaxBundle/MozartAjaxBundle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\AjaxBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MozartAjaxBundle extends Bundle {
+
+} 
