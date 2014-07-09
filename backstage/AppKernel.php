035290e8b13c9ec3b688c1e53cd460db0470f937
Alexandru Furculita <alex@rhetina.com>
2014-07-09 13:03:18 +0300

    AppKernel refresh

--- backstage/AppKernel.php
+++ backstage/AppKernel.php
@@ -9,104 +9,107 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class AppKernel extends Kernel
 {
 
-    /**
-     * @param string $environment
-     * @param bool   $debug
-     */
-    public function __construct( $environment, $debug )
-    {
-        parent::__construct( $environment, $debug );
-    }
+	/**
+	 * @param string $environment
+	 * @param bool   $debug
+	 */
+	public function __construct( $environment, $debug )
+	{
+		parent::__construct( $environment, $debug );
+	}
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'mozart';
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'mozart';
+	}
 
-    /**
-     * @return array|mixed|void
-     */
-    public function registerBundles()
-    {
-        $this->registerThemeMozartBundles();
+	/**
+	 * @return array|mixed|void
+	 */
+	public function registerBundles()
+	{
+		$this->registerThemeMozartBundles();
 
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            // load core modules
-            new Mozart\Bundle\NucleusBundle\MozartNucleusBundle(),
-            // load core modules responsible for WordPress entities
-            new Mozart\Bundle\BlogBundle\MozartBlogBundle(),
-            new Mozart\Bundle\CommentBundle\MozartCommentBundle(),
-            new Mozart\Bundle\OptionBundle\MozartOptionBundle(),
-            new Mozart\Bundle\PostBundle\MozartPostBundle(),
-            new Mozart\Bundle\ShortcodeBundle\MozartShortcodeBundle(),
-            new Mozart\Bundle\TaxonomyBundle\MozartTaxonomyBundle(),
-            new Mozart\Bundle\ThemeBundle\MozartThemeBundle(),
-            new Mozart\Bundle\UserBundle\MozartUserBundle(),
-            new Mozart\Bundle\WidgetBundle\MozartWidgetBundle(),
-            // load UI components
-            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
-            new Mozart\UI\WebIconBundle\MozartWebIconBundle()
-        );
+		$bundles = array(
+			new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+			new Symfony\Bundle\TwigBundle\TwigBundle(),
+			new Symfony\Bundle\MonologBundle\MonologBundle(),
+			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+			// load core modules
+			new Mozart\Bundle\NucleusBundle\MozartNucleusBundle(),
+			// load core modules responsible for WordPress entities
+			new Mozart\Bundle\BlogBundle\MozartBlogBundle(),
+			new Mozart\Bundle\CacheBundle\MozartCacheBundle(),
+			new Mozart\Bundle\CommentBundle\MozartCommentBundle(),
+			new Mozart\Bundle\FieldBundle\MozartFieldBundle(),
+			new Mozart\Bundle\MediaBundle\MozartMediaBundle(),
+			new Mozart\Bundle\MenuBundle\MozartMenuBundle(),
+			new Mozart\Bundle\OptionBundle\MozartOptionBundle(),
+			new Mozart\Bundle\PostBundle\MozartPostBundle(),
+			new Mozart\Bundle\ShortcodeBundle\MozartShortcodeBundle(),
+			new Mozart\Bundle\TaxonomyBundle\MozartTaxonomyBundle(),
+			new Mozart\Bundle\ThemeBundle\MozartThemeBundle(),
+			new Mozart\Bundle\UserBundle\MozartUserBundle(),
+			new Mozart\Bundle\WidgetBundle\MozartWidgetBundle(),
+			// load UI components
+			new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle()
+		);
 
-        $bundles = apply_filters( 'register_mozart_bundle', $bundles );
+		$bundles = apply_filters( 'register_mozart_bundle', $bundles );
 
-        if ( in_array( $this->getEnvironment(), array( 'dev', 'test' ) ) ) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-        //    $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+		if (in_array( $this->getEnvironment(), array( 'dev', 'test' ) )) {
+			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+			//    $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+		}
 
-        return $bundles;
-    }
+		return $bundles;
+	}
 
-    /**
-     * )
-     */
-    public function registerThemeMozartBundles()
-    {
-        // TODO: what if /themedir/vendor/autoload.php does not exist
-        include_once get_template_directory() . '/vendor/autoload.php';
+	/**
+	 * )
+	 */
+	public function registerThemeMozartBundles()
+	{
+		// TODO: what if /themedir/vendor/autoload.php does not exist
+		include_once get_template_directory() . '/vendor/autoload.php';
 
-        // TODO: what if /themedir/composer.json does not exist
-        $themeConfig = json_decode( file_get_contents( get_template_directory() . '/composer.json' ), true );
+		// TODO: what if /themedir/composer.json does not exist
+		$themeConfig = json_decode( file_get_contents( get_template_directory() . '/composer.json' ), true );
 
-        if ( isset( $themeConfig['scripts']['pre-initialize-mozart-bundles'] ) ) {
-            foreach ( (array)$themeConfig['scripts']['pre-initialize-mozart-bundles'] as $script ) {
-                call_user_func( $script );
-            }
-        }
-    }
+		if (isset( $themeConfig['scripts']['pre-initialize-mozart-bundles'] )) {
+			foreach ((array)$themeConfig['scripts']['pre-initialize-mozart-bundles'] as $script) {
+				call_user_func( $script );
+			}
+		}
+	}
 
-    /**
-     * @param LoaderInterface $loader
-     */
-    public function registerContainerConfiguration( LoaderInterface $loader )
-    {
-        $loader->load( __DIR__ . '/config/config_' . $this->getEnvironment() . '.yml' );
-    }
+	/**
+	 * @param LoaderInterface $loader
+	 */
+	public function registerContainerConfiguration( LoaderInterface $loader )
+	{
+		$loader->load( __DIR__ . '/config/config_' . $this->getEnvironment() . '.yml' );
+	}
 
-    /**
-     * @return string
-     */
-    public function getCacheDir()
-    {
-        return WP_CONTENT_DIR . '/Mozart/Cache/' . $this->environment;
-    }
+	/**
+	 * @return string
+	 */
+	public function getCacheDir()
+	{
+		return WP_CONTENT_DIR . '/Mozart/Cache/' . $this->environment;
+	}
 
-    /**
-     * @return string
-     */
-    public function getLogDir()
-    {
-        return WP_CONTENT_DIR . '/Mozart/Logs';
-    }
+	/**
+	 * @return string
+	 */
+	public function getLogDir()
+	{
+		return WP_CONTENT_DIR . '/Mozart/Logs';
+	}
 }
