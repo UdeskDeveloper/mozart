2732258be1298a3bcac61c8e742d188d3e4f5f25
Alexandru Furculita <alex@rhetina.com>
2014-07-09 12:59:00 +0300

    mozart.php code refresh

--- mozart.php
+++ mozart.php
@@ -15,33 +15,32 @@ Version: 1.0.0
  */
 function mozart_start_concert()
 {
-    require_once __DIR__ . '/backstage/bootstrap.php';
+	require_once __DIR__ . '/backstage/bootstrap.php';
 }
 
 add_action('plugins_loaded', 'mozart_start_concert');
 
 if (is_admin()) {
-    function mozart_wordpress_activation_hook()
-    {
-        /*
-         * TODO: add activation hook
-         *
-         * check for Symfony 2 requirements, use /backstage/check.php
-         * check if the current theme has Mozart support.
-         * For a theme to have Mozart support, it needs to have a composer.json
-         * in its directory root, in which specifies a file to be autoloaded,
-         * file that contains stuff to be executed when the bundles are initialized,
-         * like adding a filter for "register_mozart_bundle" to register the theme bundles
-         *
-         */
+	function mozart_wordpress_activation_hook()
+	{
+		/*
+		 * TODO: add activation hook
+		 *
+		 * check for Symfony 2 requirements, use /backstage/check.php
+		 * check if the current theme has Mozart support.
+		 * For a theme to have Mozart support, it needs to have a composer.json
+		 * in its directory root, in which specifies a file to be autoloaded,
+		 * file that contains stuff to be executed when the bundles are initialized,
+		 * like adding a filter for "register_mozart_bundle" to register the theme bundles
+		 *
+		 */
 
-    }
-    register_activation_hook(__FILE__, 'mozart_wordpress_activation_hook');
+	}
+	register_activation_hook(__FILE__, 'mozart_wordpress_activation_hook');
 
-    function mozart_wordpress_uninstall_hook()
-    {
-        // TODO: add unninstall hook
-    }
-    register_uninstall_hook(__FILE__, 'mozart_wordpress_uninstall_hook');
+	function mozart_wordpress_uninstall_hook()
+	{
+		// TODO: add unninstall hook
+	}
+	register_uninstall_hook(__FILE__, 'mozart_wordpress_uninstall_hook');
 }
-dynamic_sidebar();
