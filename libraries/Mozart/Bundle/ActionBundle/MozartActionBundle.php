25e3850df23292dc736207c90219b10cc8f011b4
Alexandru Furculita <alex@rhetina.com>
2014-07-09 13:35:09 +0300

    ActionBundle added

---
+++ libraries/Mozart/Bundle/ActionBundle/MozartActionBundle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\ActionBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MozartActionBundle extends Bundle {
+
+} 
