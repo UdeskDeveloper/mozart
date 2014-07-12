2cd3e34b906ccf6d9f48bb6a6cbd4e2c04baedb7
Alexandru Furculita <alex@rhetina.com>
2014-07-12 14:12:13 +0300

    Added PluginEvents class

---
+++ libraries/Mozart/Bundle/PluginBundle/PluginEvents.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\PluginBundle;
+
+
+class PluginEvents
+{
+    const PLUGIN_ACTIVATION = 'wordpress.plugin_activation';
+    const PLUGIN_DEACTIVATION = 'wordpress.plugin_deactivation';
+    const PLUGIN_UNINSTALL = 'wordpress.plugin_uninstall';
+} 
