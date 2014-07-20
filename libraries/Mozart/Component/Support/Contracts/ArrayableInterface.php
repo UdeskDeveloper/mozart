9a93acee5e6cb5259ee42dafa6c45f1ed9df9383
Alexandru Furculita <alex@rhetina.com>
2014-07-20 22:09:58 +0300

    Support files added

---
+++ libraries/Mozart/Component/Support/Contracts/ArrayableInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Mozart\Component\Support\Contracts;
+
+interface ArrayableInterface {
+
+	/**
+	 * Get the instance as an array.
+	 *
+	 * @return array
+	 */
+	public function toArray();
+
+}
