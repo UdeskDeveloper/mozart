d8995bb3bcfabce9a21cd27c64681078fdee8c68
Alexandru Furculita <alex@rhetina.com>
2014-07-22 13:50:57 +0300

    Simplified OptionSection

--- libraries/Mozart/Component/Option/Section/OptionSection.php
+++ libraries/Mozart/Component/Option/Section/OptionSection.php
@@ -5,26 +5,23 @@
 
 namespace Mozart\Component\Option\Section;
 
-use Traversable;
+use Mozart\Component\Support\Str;
 
 /**
  * Class OptionSection
  * @package Mozart\Component\Option\Section
  */
-class OptionSection implements SectionInterface, \ArrayAccess
+class OptionSection implements SectionInterface
 {
-
-    /**
-     * @var array
-     */
-    private $section;
-
-    /**
-     * @return array
-     */
     public function __construct()
     {
-        $this->section               = array();
+    }
+
+    public function getAlias() {
+        $className     = get_class( $this );
+        $classBaseName = substr( strrchr( $className, '\\' ), 1 );
+
+        return Str::snake( $classBaseName );
     }
 
     /**
@@ -33,6 +30,7 @@ class OptionSection implements SectionInterface, \ArrayAccess
     public function getConfiguration()
     {
         $conf = array(
+            'id' => $this->getAlias(),
             'icon' => $this->getIcon(),
             'title' => $this->getTitle(),
             'desc' => $this->getDescription(),
@@ -72,7 +70,9 @@ class OptionSection implements SectionInterface, \ArrayAccess
      */
     public function getTitle()
     {
-        return '';
+        $alias = $this->getAlias();
+
+        return translate( Str::studly($alias) );
     }
 
     /**
@@ -94,94 +94,4 @@ class OptionSection implements SectionInterface, \ArrayAccess
         return array();
     }
 
-    /**
-     * (PHP 5 &gt;= 5.0.0)<br/>
-     * Whether a offset exists
-     *
-     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
-     *
-     * @param mixed $offset <p>
-     *                      An offset to check for.
-     *                      </p>
-     *
-     * @return boolean true on success or false on failure.
-     *                 </p>
-     *                 <p>
-     *                 The return value will be casted to boolean if non-boolean was returned.
-     */
-    public function offsetExists($offset)
-    {
-        return isset( $this->section[$offset] );
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.0.0)<br/>
-     * Offset to retrieve
-     *
-     * @link http://php.net/manual/en/arrayaccess.offsetget.php
-     *
-     * @param mixed $offset <p>
-     *                      The offset to retrieve.
-     *                      </p>
-     *
-     * @return mixed Can return all value types.
-     */
-    public function offsetGet($offset)
-    {
-        return isset( $this->section[$offset] ) ? $this->section[$offset] : null;
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.0.0)<br/>
-     * Offset to set
-     *
-     * @link http://php.net/manual/en/arrayaccess.offsetset.php
-     *
-     * @param mixed $offset <p>
-     *                      The offset to assign the value to.
-     *                      </p>
-     * @param mixed $value  <p>
-     *                      The value to set.
-     *                      </p>
-     *
-     * @return void
-     */
-    public function offsetSet($offset, $value)
-    {
-        if (is_null( $offset )) {
-            $this->section[] = $value;
-        } else {
-            $this->section[$offset] = $value;
-        }
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.0.0)<br/>
-     * Offset to unset
-     *
-     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
-     *
-     * @param mixed $offset <p>
-     *                      The offset to unset.
-     *                      </p>
-     *
-     * @return void
-     */
-    public function offsetUnset($offset)
-    {
-        unset( $this->section[$offset] );
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.0.0)<br/>
-     * Retrieve an external iterator
-     *
-     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return Traversable An instance of an object implementing <b>Iterator</b> or
-     *                     <b>Traversable</b>
-     */
-    public function getIterator()
-    {
-        // TODO: Implement getIterator() method.
-    }
 }
