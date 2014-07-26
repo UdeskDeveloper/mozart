a6e5b8db23a36ab5bc134655a13cb28604311885
Alexandru Furculita <alex@rhetina.com>
2014-07-26 09:05:16 +0300

    ClearCache added

--- libraries/Mozart/Bundle/PluginBundle/MozartPluginBundle.php
+++ libraries/Mozart/Bundle/PluginBundle/MozartPluginBundle.php
@@ -6,6 +6,7 @@
 namespace Mozart\Bundle\PluginBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -23,7 +24,18 @@ class MozartPluginBundle extends Bundle
 
     public function boot()
     {
-        add_action( 'init', array( $this->container->get('mozart.plugin.manager'), 'init' ) );
+        add_action( 'init', array( $this->container->get( 'mozart.plugin.manager' ), 'init' ) );
+        add_action( 'deactivated_plugin', array( $this, 'clearCache' ), 10, 2 );
+        add_action( 'activated_plugin', array( $this, 'clearCache' ), 10, 2 );
+    }
+
+    public function clearCache($plugin, $network_activation)
+    {
+        $filesystem = new Filesystem();
+
+        if ($filesystem->exists( $this->container->getParameter( 'kernel.cache_dir' ) )) {
+            $filesystem->remove( $this->container->getParameter( 'kernel.cache_dir' ) );
+        }
     }
 
 }
