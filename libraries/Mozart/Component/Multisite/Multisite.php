2538947a70ad76f32accf3685edd06a582b155a2
Alexandru Furculita <alex@rhetina.com>
2014-07-29 21:15:26 +0300

    Created Multisite component

---
+++ libraries/Mozart/Component/Multisite/Multisite.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Component\Multisite;
+
+
+class Multisite
+{
+    /**
+     * Whether Multisite support is enabled
+     *
+     * @since 3.0.0
+     *
+     * @return bool True if multisite is enabled, false otherwise.
+     */
+    function isEnabled()
+    {
+        if (defined( 'MULTISITE' )) {
+            return MULTISITE;
+        }
+
+        if (defined( 'SUBDOMAIN_INSTALL' ) || defined( 'VHOST' ) || defined( 'SUNRISE' )) {
+            return true;
+        }
+
+        return false;
+    }
+} 
