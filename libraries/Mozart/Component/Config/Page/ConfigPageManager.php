25e02f48b012199fa129f82b0a60f329fab1ffd2
Alexandru Furculita <alex@rhetina.com>
2014-08-01 11:08:44 +0300

    Added support for config pages

---
+++ libraries/Mozart/Component/Config/Page/ConfigPageManager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Component\Config\Page;
+
+
+class ConfigPageManager {
+    private $pages;
+
+    public function __construct() {
+        $this->pages = array();
+    }
+
+
+}
