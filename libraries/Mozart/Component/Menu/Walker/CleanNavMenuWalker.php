d58c7efcde7bd5cb979f3c4acb78160bf0f99469
Alexandru Furculita <alex@rhetina.com>
2014-08-08 13:59:38 +0300

    Added Menu Walkers

---
+++ libraries/Mozart/Component/Menu/Walker/CleanNavMenuWalker.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Component\Menu\Walker;
+
+
+class CleanNavMenuWalker extends \Walker_Nav_Menu
+{
+
+    /**
+     * Start the element output.
+     *
+     * @see Walker::start_el()
+     *
+     * @since 3.0.0
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item   Menu item data object.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     * @param int    $id     Current item ID.
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        /**
+         * Filter the ID applied to a menu item's <li>.
+         *
+         * @since 3.0.1
+         *
+         * @see wp_nav_menu()
+         *
+         * @param string $menu_id The ID that is applied to the menu item's <li>.
+         * @param object $item    The current menu item.
+         * @param array  $args    An array of wp_nav_menu() arguments.
+         */
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+        $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        $output .= $indent . '<li' . $id .'>';
+
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+        /**
+         * Filter the HTML attributes applied to a menu item's <a>.
+         *
+         * @since 3.6.0
+         *
+         * @see wp_nav_menu()
+         *
+         * @param array $atts {
+         *     The HTML attributes applied to the menu item's <a>, empty strings are ignored.
+         *
+         *     @type string $title  Title attribute.
+         *     @type string $target Target attribute.
+         *     @type string $rel    The rel attribute.
+         *     @type string $href   The href attribute.
+         * }
+         * @param object $item The current menu item.
+         * @param array  $args An array of wp_nav_menu() arguments.
+         */
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+        $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        /** This filter is documented in wp-includes/post-template.php */
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        /**
+         * Filter a menu item's starting output.
+         *
+         * The menu item's starting output only includes $args->before, the opening <a>,
+         * the menu item's title, the closing </a>, and $args->after. Currently, there is
+         * no filter for modifying the opening and closing <li> for a menu item.
+         *
+         * @since 3.0.0
+         *
+         * @see wp_nav_menu()
+         *
+         * @param string $item_output The menu item's starting HTML output.
+         * @param object $item        Menu item data object.
+         * @param int    $depth       Depth of menu item. Used for padding.
+         * @param array  $args        An array of wp_nav_menu() arguments.
+         */
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+
+} 
