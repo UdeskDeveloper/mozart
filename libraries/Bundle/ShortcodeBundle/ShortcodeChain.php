427110084e9abb4d8f019f94134f55169f4cf59e
Alexandru Furculita <alex@rhetina.com>
2014-08-11 20:00:28 +0300

    Formatting issues solved

--- libraries/Bundle/ShortcodeBundle/ShortcodeChain.php
+++ libraries/Bundle/ShortcodeBundle/ShortcodeChain.php
@@ -156,16 +156,17 @@ class ShortcodeChain
         $text = preg_replace( "/[\x{00a0}\x{200b}]+/u", " ", $text );
         if (preg_match_all( $pattern, $text, $match, PREG_SET_ORDER )) {
             foreach ($match as $m) {
-                if (!empty( $m[1] ))
+                if (!empty( $m[1] )) {
                     $atts[strtolower( $m[1] )] = stripcslashes( $m[2] );
-                elseif (!empty( $m[3] )) {
+                } elseif (!empty( $m[3] )) {
                     $atts[strtolower( $m[3] )] = stripcslashes( $m[4] );
-                } elseif (!empty( $m[5] ))
+                } elseif (!empty( $m[5] )) {
                     $atts[strtolower( $m[5] )] = stripcslashes( $m[6] );
-                elseif (isset( $m[7] ) and strlen( $m[7] ))
+                } elseif (isset( $m[7] ) && strlen( $m[7] ) > 0) {
                     $atts[] = stripcslashes( $m[7] );
-                elseif (isset( $m[8] ))
+                } elseif (isset( $m[8] )) {
                     $atts[] = stripcslashes( $m[8] );
+                }
             }
         }
 
