6edafb9a8ba638e248b9197f2aa0650318b21341
Alexandru Furculita <alex@rhetina.com>
2014-08-16 10:48:21 +0300

    Bootstrap fixes

--- backstage/bootstrap.php
+++ backstage/bootstrap.php
@@ -11,7 +11,7 @@ if (defined( 'WP_DEBUG' ) && WP_DEBUG) {
 if (false === $debug) {
     $loader = require_once __DIR__ . '/bootstrap.php.cache';
 } else {
-    $loader = require_once __DIR__.'/autoload.php';
+    $loader = require_once __DIR__ . '/autoload.php';
 }
 
 // Use APC for autoloading to improve performance.
@@ -21,17 +21,15 @@ if (defined( 'WP_DEBUG' ) && false === WP_DEBUG && extension_loaded( 'apc' )) {
     $apcLoader->register( true );
 }
 
-//if (true === $debug) {
-//    Symfony\Component\Debug\Debug::enable(  );
-//}
+if (true === $debug) {
+//    Symfony\Component\Debug\Debug::enable(0);
+}
 
 require_once __DIR__ . '/MozartKernel.php';
 //require_once __DIR__.'/MozartCache.php';
 
 $kernel = new MozartKernel( $environment, $debug );
-if (false === $debug) {
-    $kernel->loadClassCache();
-}
+$kernel->loadClassCache();
 
 // $kernel = new MozartCache($kernel);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
@@ -41,11 +39,11 @@ $kernel->boot();
 
 Mozart::setContainer( $kernel->getContainer() );
 
-do_action('mozart.init');
+do_action( 'mozart.init' );
 
 add_action(
     'wp_loader',
-    function () use ( $kernel ) {
+    function () use ($kernel) {
         $kernel->shutdown();
     },
     999
