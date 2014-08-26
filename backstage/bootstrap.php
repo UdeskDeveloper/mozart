125dc06d1932a25d023830e49d4d71f68f6bf647
Alexandru Furculita <alex@rhetina.com>
2014-08-27 00:12:07 +0300

    Clearer class usage in bootstrap

--- backstage/bootstrap.php
+++ backstage/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-use Mozart\Bundle\NucleusBundle\MozartEvents;
-
 $environment = 'prod';
 $debug = false;
 
@@ -42,7 +40,7 @@ $kernel->boot();
 Mozart::setContainer( $kernel->getContainer() );
 
 do_action( 'mozart.init' );
-$kernel->getContainer()->get( 'event_dispatcher' )->dispatch( MozartEvents::INIT );
+$kernel->getContainer()->get( 'event_dispatcher' )->dispatch( Mozart\Bundle\NucleusBundle\MozartEvents::INIT );
 
 add_action(
     'wp_loader',
