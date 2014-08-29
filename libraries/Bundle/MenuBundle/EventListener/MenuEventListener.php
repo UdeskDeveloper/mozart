90dac7a17fe3450e55f49ce1a299a140327276d6
Alexandru Furculita <alex@rhetina.com>
2014-08-29 20:16:02 +0300

    Added event listener to menuBundle

---
+++ libraries/Bundle/MenuBundle/EventListener/MenuEventListener.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\MenuBundle\EventListener;
+
+use Mozart\Component\Menu\Event\MenuEvent;
+use Mozart\Component\Menu\MenuEvents;
+
+class MenuEventListener
+{
+	public function onKernelBoot()
+	{
+		add_filter( 'custom_menu_order', '__return_true' );
+		add_filter(
+			MenuEvents::ORDER,
+			function ($menuOrder) {
+				$event = new MenuEvent();
+				$event->setAdminMenuOrder( $menuOrder );
+				/** @var MenuEvent $event */
+				$event = \Mozart::dispatch( MenuEvents::ORDER, $event );
+
+				return $event->getAdminMenuOrder();
+			},
+			0
+		);
+
+		/**
+		 * @see add_menu_classes
+		 */
+		add_filter(
+			MenuEvents::FILTER,
+			function ($menu) {
+				$event = new MenuEvent();
+				$event->setMenu( $menu );
+				/** @var MenuEvent $event */
+				$event = \Mozart::dispatch( MenuEvents::FILTER, $event );
+
+				return $event->getMenu();
+			},
+			10,
+			1
+		);
+
+		add_filter(
+			'admin_print_scripts',
+			function () {
+				wp_enqueue_script(
+					'mozart-menu-delete',
+					plugins_url( '/mozart/public/bundles/mozart/menu/js/delete.js' )
+				);
+			}
+		);
+	}
+} 
--- libraries/Bundle/MenuBundle/MozartMenuBundle.php
+++ libraries/Bundle/MenuBundle/MozartMenuBundle.php
@@ -5,8 +5,6 @@
 
 namespace Mozart\Bundle\MenuBundle;
 
-use Mozart\Component\Menu\Event\MenuEvent;
-use Mozart\Component\Menu\MenuEvents;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -20,45 +18,7 @@ class MozartMenuBundle extends Bundle
 
     public function boot()
     {
-        add_filter( 'custom_menu_order', '__return_true' );
-        add_filter(
-            MenuEvents::ORDER,
-            function ($menuOrder) {
-                $event = new MenuEvent();
-                $event->setAdminMenuOrder( $menuOrder );
-                /** @var MenuEvent $event */
-                $event = \Mozart::dispatch( MenuEvents::ORDER, $event );
-                return $event->getAdminMenuOrder();
-            },
-            0
-        );
 
-		/**
-         * @see add_menu_classes
-         */
-        add_filter(
-            MenuEvents::FILTER,
-            function ($menu) {
-                $event = new MenuEvent();
-                $event->setMenu( $menu );
-                /** @var MenuEvent $event */
-                $event = \Mozart::dispatch( MenuEvents::FILTER, $event );
-
-                return $event->getMenu();
-            },
-            10,
-            1
-        );
-		
-        add_filter(
-            'admin_print_scripts',
-            function () {
-                wp_enqueue_script(
-                    'mozart-menu-delete',
-                    plugins_url( '/mozart/public/bundles/mozart/menu/js/delete.js' )
-                );
-            }
-        );
     }
 
 }
