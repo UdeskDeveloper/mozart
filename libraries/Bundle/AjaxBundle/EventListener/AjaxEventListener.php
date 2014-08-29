b877c2628258a9c6522a0c7ee1fe7d049da18958
Alexandru Furculita <alex@rhetina.com>
2014-08-29 16:24:59 +0300

    Register AjaxEventListener

---
+++ libraries/Bundle/AjaxBundle/EventListener/AjaxEventListener.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright 2014 Alexandru Furculita <alex@rhetina.com>
+ */
+
+namespace Mozart\Bundle\AjaxBundle\EventListener;
+
+use Mozart\Component\Ajax\Ajax;
+
+class AjaxEventListener
+{
+	public function onApplicationInit()
+	{
+		add_action(
+			'wp_head',
+			function () {
+				Ajax::installScript( 'Mozart' );
+			}
+		);
+	}
+} 
